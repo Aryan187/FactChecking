a2d61c481c825491482acd0275345e08446656bb
Aryan187 <mt1180744@iitd.ac.in>
2021-03-11 20:48:12 +0530

    original sentences added

---
+++ data/original_random.py
@@ -0,0 +1,16 @@
+import json
+import random
+
+all_data = open('full_original.json')
+data = json.load(all_data)
+
+sample_data = open('random_all.json')
+sample = json.load(sample_data)
+
+rand_clean = {}
+for k in sample:
+	rand_clean[k] = data[k]
+out_json = json.dumps(rand_clean,indent=1)
+
+with open('random_original.json',"w") as out:
+	out.write(out_json)
